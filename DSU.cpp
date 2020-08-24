c91012871c167e4f323904243f8614300b67fce2
Ayushman Mathur <68735227+invictus-123@users.noreply.github.com>
2020-08-25 01:44:05 +0530

    Create DSU.cpp

---
+++ DSU.cpp
@@ -0,0 +1,17 @@
+const int MAX = 1e5 + 10;
+int par[MAX], sz[MAX];
+void init(int v) {
+	par[v] = v;
+	sz[v] = 1;
+}
+int find(int v) {
+	return v == par[v] ? v : par[v] = find(par[v]);
+}
+void join(int u, int v) {
+	u = find(u), v = find(v);
+	if(u != v) {
+		if(sz[u] < sz[v]) swap(u, v);
+		par[v] = u;
+		sz[u] += sz[v];
+	}
+}
