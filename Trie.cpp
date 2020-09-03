a29e11234120997212169662c1073a36b3e67bce
Ayushman Mathur <68735227+invictus-123@users.noreply.github.com>
2020-09-04 01:44:39 +0530

    Update Trie.cpp

--- Trie.cpp
+++ Trie.cpp
@@ -1,3 +1,7 @@
+// Time Complexity : O(bits)
+// Verification : https://codeforces.com/contest/923/submission/91750294
+// Modifications : Tarun Singhania (bhayia)
+// Declaration : Wrtie root = new TrieNode() in main. 
 struct TrieNode {
 	TrieNode *bit[2];
 	int cnt;
@@ -7,22 +11,25 @@ struct TrieNode {
 	}
 };
 TrieNode *root;
-// write root = new TrieNode(); in main!!!
 void add(int x) {
 	TrieNode* cur = root;
 	for(int i = 30; i >= 0; i --) {
 		int b = (x >> i) & 1;
 		if(!cur->bit[b]) cur->bit[b] = new TrieNode();
-		cur = cur->bit[b];
 		cur->cnt ++;
+		cur = cur->bit[b];
 	}
+	cur->cnt ++;
 }
 void remove(int x) {
 	TrieNode *cur = root;
 	for(int i = 30; i >= 0; i --) {
 		int b = (x >> i) & 1;
-		cur = cur->bit[b];
+		TrieNode *temp = cur->bit[b];
+		if(cur->bit[b]->cnt - 1 == 0) cur->bit[b] = NULL;
 		cur->cnt --;
+		if(cur && cur->cnt == 0) free(cur);
+		cur = temp;
 	}
 }
 int query(int x) {
@@ -30,7 +37,7 @@ int query(int x) {
 	int ans = 0;
 	for(int i = 30; i >= 0; i --) {
 		int b = (x >> i) & 1;
-		if(cur->bit[b ^ 1] != NULL && cur->bit[b ^ 1]->cnt > 0) {
+		if(cur->bit[b] != NULL && cur->bit[b]->cnt > 0) {
 			
 		}
 		else {
