5e3de23fd9051173401465c6e2cc7522849a3d9b
Ayushman Mathur <68735227+invictus-123@users.noreply.github.com>
2020-09-05 17:55:24 +0530

    Create Z Function.cpp

---
+++ Z Function.cpp
@@ -0,0 +1,13 @@
+// Time Complexity : O(n)
+// Verification : https://codeforces.com/contest/535/submission/91938635
+// Declaration : Returns a vector representing the z function of the string
+vector <int> Z_Function(string &s) {
+	int n = s.size();
+	vector <int> z(n, 0);
+	for(int i = 1, l = 0, r = 0; i < n; i ++) {
+		if(i <= r) z[i] = min(r - i + 1, z[i - l]);
+		while(i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i] ++;
+		if(i + z[i] - 1 > r) l = i, r = i + z[i] - 1;
+	}
+	return z;
+}
