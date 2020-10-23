c9c6f3be614749d5c0b0374974c0e6339359945c
Ayushman Mathur <68735227+invictus-123@users.noreply.github.com>
2020-10-23 17:17:56 +0530

    Update nCr.cpp

--- nCr.cpp
+++ nCr.cpp
@@ -1,11 +1,5 @@
-ll fact[MAX];
-void calculate() {
-	fact[0] = 1;
-	for(int i = 1; i < MAX; i ++) {
-		fact[i] = (fact[i - 1] * i) % MOD;
-	}
-}
-// call calculate() in main function!!!
+// don't forget to call calculate in main
+ll fact[MAX], invfact[MAX];
 ll modExp(ll b, ll p) {
 	ll res = 1;
 	while(p > 0) {
@@ -17,6 +11,20 @@ ll modExp(ll b, ll p) {
 ll modInverse(ll n) {
 	return modExp(n, MOD - 2); 
 }
+void calculate() {
+	fact[0] = 1;
+	for(int i = 1; i < MAX; i ++) {
+		fact[i] = (fact[i - 1] * i) % MOD;
+	}
+	invfact[MAX - 1] = modInverse(fact[MAX - 1]);
+	for(int i = MAX - 2; i >= 0; i --) {
+		invfact[i] = ((i + 1) * invfact[i + 1]) % MOD;
+	}
+}
 ll nCr(ll n, ll r) {
-	return (fact[n] * modInverse(fact[n - r]) % MOD * modInverse(fact[r]) % MOD) % MOD;
+	if(n < 0 || r < 0 || n < r) return 0;
+	ll ret = fact[n];
+	ret = (ret * invfact[n - r]) % MOD;
+	ret = (ret * invfact[r]) % MOD;
+	return ret;
 }
